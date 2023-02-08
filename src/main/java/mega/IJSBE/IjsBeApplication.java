905d130167dbd168eecd5d5cb507f4e8dfcf36d9
MoonSangWon <kuim75@naver.com>
2023-02-08 14:43:05 +0900

    test

---
+++ src/main/java/mega/IJSBE/IjsBeApplication.java
@@ -0,0 +1,13 @@
+package mega.IJSBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjsBeApplication.class, args) ;
+	}
+
+}
---
+++ src/test/java/mega/IJSBE/IjsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mega.IJSBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
