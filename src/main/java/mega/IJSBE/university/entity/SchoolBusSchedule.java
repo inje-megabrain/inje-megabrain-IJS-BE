c347b4f081258743ebeb1d72238eada277d3af6c
MoonSangWon <kuim75@naver.com>
2023-02-09 00:26:13 +0900

    feat : 교내 통학 버스 entity 추가

---
+++ src/main/java/mega/IJSBE/university/entity/SchoolBusSchedule.java
@@ -0,0 +1,24 @@
+package mega.IJSBE.university.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="school_bus_schedules")
+public class SchoolBusSchedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date departureTime;
+}
