1978051082f9fafa8ee3fc7c4eced5ebc639bc6c
MoonSangWon <kuim75@naver.com>
2023-02-09 12:02:52 +0900

    fix

--- src/main/java/mega/IJSBE/university/service/SubjectService.java
+++ src/main/java/mega/IJSBE/university/service/SubjectService.java
@@ -3,6 +3,7 @@ package mega.IJSBE.university.service;
 import mega.IJSBE.university.entity.UniversityNonsubject;
 import mega.IJSBE.university.repository.NonSubjectsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -21,7 +22,7 @@ public class SubjectService {
     }
     /************ 현재 시간을 지난 비교과 프로그램은 담지 않음 ***************/
     public List<UniversityNonsubject> findToSubject(){
-        List<UniversityNonsubject> sub = nonSubjectsRepository.findAll();
+        List<UniversityNonsubject> sub = nonSubjectsRepository.findAll(Sort.by("endAt").descending());
         List<UniversityNonsubject> list = new ArrayList<>();
         for(UniversityNonsubject i : sub){
             LocalDate end = LocalDate.ofInstant(i.getEndAt().toInstant(), ZoneId.systemDefault());
